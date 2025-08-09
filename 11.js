f238b8e474e5053eb431e74a01ee180cbb5ea0f9
maxfrisson <>
2025-08-09 11:29:52 +0300

    fix 11

--- 11.js
+++ 11.js
@@ -4,3 +4,6 @@ console.log(solution([1, 2, 10, 50, 5]));
 console.log(solution(null));
 console.log(solution([]));
 console.log(solution([20, 2, 10]));
+console.log(solution([20, 2, 10]));
+console.log(solution([20, 2, 10]));
+console.log(solution([20, 2, 10]));
